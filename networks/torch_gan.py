e0f8328aa97b68049526361497c8bca0cdc49d59
Niklas Penzel <niklas.penzel@uni-jena.de>
2020-02-23 13:33:50 +0100

    finally got only conv and upsampling working

--- networks/torch_gan.py
+++ networks/torch_gan.py
@@ -14,20 +14,23 @@ import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
+from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from IPython.display import HTML
 import os
 from PIL import Image
+from datetime import datetime
+from tqdm import tqdm
 
 """
 Hyper-Parameters
 """
 # Set random seed for reproducibility
-manualSeed = 420
-#manualSeed = random.randint(1, 10000) # use if you want new results
-#print("Random Seed: ", manualSeed)
+#manualSeed = 420
+manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
@@ -41,7 +44,7 @@ batch_size = 128
 #   size using a transformer.
 image_size = 64
 # Number of channels in the training images. For color images this is 3
-nc = 4
+nc = 3
 # Size of z latent vector (i.e. size of generator input)
 nz = 100
 # Size of feature maps in generator
@@ -49,13 +52,17 @@ ngf = 64
 # Size of feature maps in discriminator
 ndf = 64
 # Number of training epochs
-num_epochs = 15
+num_epochs = 30
 # Learning rate for optimizers
 lr = 0.0002
 # Beta1 hyperparam for Adam optimizers
 beta1 = 0.5
 # Number of GPUs available. Use 0 for CPU mode.
 ngpu = 0
+
+d_thresh = 0.1
+
+label_smooth = 0.9
 
 
 # custom weights initialization called on netG and netD
@@ -67,7 +74,14 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-
+def to_variable(x):
+    '''
+    convert a tensor to a variable
+    with gradient tracking
+    '''
+    if torch.cuda.is_available():
+        x = x .cuda()
+    return Variable(x)
 
 # rgba loader
 def pil_loader(path):
@@ -77,11 +91,13 @@ def pil_loader(path):
 
 
 class View(nn.Module):
-    def __init__(self, *shape):
-        super(View, self).__init__()
+    def __init__(self, shape):
+        super().__init__()
         self.shape = shape
     def forward(self, input):
-        return input.view(*shape)
+        b_size = input.size(0)
+        shape = (b_size, *self.shape)
+        return input.view(shape)
 
 
 class PrintLayer(nn.Module):
@@ -91,6 +107,7 @@ class PrintLayer(nn.Module):
     def forward(self, x):
         # Do your print / debug stuff here
         print(x.size())
+        #print(x)
         return x
 
 
@@ -103,39 +120,45 @@ class Generator(nn.Module):
         self.main = nn.Sequential(
             # input is Z, going into a convolution
             #nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.Conv2d(nz, ngf*8, 1, 1, bias=False),
-            nn.Upsample(scale_factor=4, mode='bilinear'),
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(ngf*8, ngf*8, 3, 1, padding=0),
+            #nn.Upsample(scale_factor=8, mode='nearest'),
+            #PrintLayer(),
+            #nn.ReflectionPad2d(1),
+            nn.Conv2d(nz, ngf*8*4*4, 1, 1, padding=0, bias=True),
+            View( (ngf*8, 4, 4) ),
             # nn.Upsample()
+            #PrintLayer(),
             nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
             # state size. (ngf*8) x 4 x 4
             #nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='nearest'),
             nn.ReflectionPad2d(1),
-            nn.Conv2d(ngf*8, ngf*4, 3, 1, padding=0),
+            nn.Conv2d(ngf*8, ngf*4, 3, 1, padding=0, bias=False),
+            #PrintLayer(),
             nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
             # state size. (ngf*4) x 8 x 8
             #nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='nearest'),
             nn.ReflectionPad2d(1),
-            nn.Conv2d( ngf * 4, ngf * 2, 3, 1, padding=0),
+            nn.Conv2d( ngf * 4, ngf * 2, 3, 1, padding=0, bias=False),
             nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
+            #PrintLayer(),
+            nn.ReLU(inplace=True),
             # state size. (ngf*2) x 16 x 16
             #nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='nearest'),
             nn.ReflectionPad2d(1),
-            nn.Conv2d( ngf * 2, ngf, 3, 1, padding=0),
+            nn.Conv2d( ngf * 2, ngf, 3, 1, padding=0, bias=False),
+            #PrintLayer(),
             nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
+            nn.ReLU(inplace=True),
             # state size. (ngf) x 32 x 32
             #nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='nearest'),
             nn.ReflectionPad2d(1),
-            nn.Conv2d( ngf, nc, 3, 1, padding=0),
+            nn.Conv2d( ngf, nc, 3, 1, padding=0, bias=False),
+            #PrintLayer(),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -150,22 +173,32 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
+            #PrintLayer(),
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            #PrintLayer(),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.25),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            #PrintLayer(),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.25),
             # state size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            #PrintLayer(),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.25),
             # state size. (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            #PrintLayer(),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.1),
             # state size. (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            #PrintLayer(),
             nn.Sigmoid()
         )
 
@@ -180,11 +213,11 @@ def train_torch_gan():
     dataset = dset.ImageFolder(root=dataroot,
                             transform=transforms.Compose([
                                 transforms.RandomHorizontalFlip(),
-                                transforms.RandomAffine(5, translate=(0.05, 0.05), scale=None, shear=2, resample=False, fillcolor=(0,0,0,0)),
+                                transforms.RandomAffine(5, translate=(0.1, 0.1), scale=None, shear=2, resample=False, fillcolor=(255,255,255)),
                                 transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5)),
-                            ]),
-                            loader=pil_loader)
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ])
+                            )#loader=pil_loader)
     # Create the dataloader
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                             shuffle=True, num_workers=workers)
@@ -198,7 +231,7 @@ def train_torch_gan():
     plt.axis("off")
     plt.title("Training Images")
     plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-    #plt.show()
+    plt.show()
 
 
     # Create the generator
@@ -252,6 +285,13 @@ def train_torch_gan():
     D_losses = []
     iters = 0
 
+    progress = 0
+    d = datetime.now().strftime("%H-%M-%S_%d-%m-%Y")
+    progress_path = os.path.join("..", "progress_pics", d)
+    if not os.path.isdir(progress_path):
+        os.makedirs(progress_path)
+
+
     print("Starting Training Loop...")
     # For each epoch
     for epoch in range(num_epochs):
@@ -266,7 +306,7 @@ def train_torch_gan():
             # Format batch
             real_cpu = data[0].to(device)
             b_size = real_cpu.size(0)
-            label = torch.full((b_size,), real_label, device=device)
+            label = torch.full((b_size,), real_label, device=device) * label_smooth
             # Forward pass real batch through D
             output = netD(real_cpu).view(-1)
             # Calculate loss on all-real batch
@@ -291,7 +331,8 @@ def train_torch_gan():
             # Add the gradients from the all-real and all-fake batches
             errD = errD_real + errD_fake
             # Update D
-            optimizerD.step()
+            if errD.item() >= d_thresh:
+                optimizerD.step()
 
             ############################
             # (2) Update G network: maximize log(D(G(z)))
@@ -309,7 +350,7 @@ def train_torch_gan():
             optimizerG.step()
 
             # Output training stats
-            if i % 50 == 0:
+            if i % 25 == 0:
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                     % (epoch, num_epochs, i, len(dataloader),
                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
@@ -323,6 +364,8 @@ def train_torch_gan():
                 with torch.no_grad():
                     fake = netG(fixed_noise).detach().cpu()
                 img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                vutils.save_image(fake, os.path.join(progress_path, f"{progress:03d}.png"))
+                progress += 1
 
             iters += 1
 
